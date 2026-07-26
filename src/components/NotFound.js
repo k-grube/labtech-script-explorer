7f40b5473736b16785f1f8cbc24f1ee11b464a18
k-grube <grube.kevin@gmail.com>
2026-07-26 02:07:41 -0700

    feat: scaffold vite react 19 app, remove cra/redux/express stack

--- src/components/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function NotFound() {
-  return (
-    <div>Not found.</div>
-  );
-}
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
+
+export default defineConfig({
+  // gh-pages serves from /labtech-script-explorer/
+  base: '/labtech-script-explorer/',
+  plugins: [react(), nodePolyfills({include: ['events', 'stream', 'string_decoder', 'timers', 'buffer']})],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.js'],
+    globals: true,
+  },
+});
